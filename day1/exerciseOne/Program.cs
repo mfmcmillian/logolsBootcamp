669bed267ba62592ac001dff231c1330ba337893
Mcmillian <212585325@logon.ds.ge.com>
2018-05-26 10:24:14 -0400

    exerisesAdded

---
+++ day1/exerciseOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exerciseOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int statusCode = 2;
+            if(statusCode == 1)
+            {
+                Console.WriteLine("You are alive");
+            }
+            
+            else if(statusCode == 2)
+            {
+                Console.WriteLine("You are a zombie");
+            }
+            
+            else if(statusCode == 3)
+            {
+                Console.WriteLine("You are dead");
+            }
+            
+            else(statusCode == 4)
+            {
+                Console.WriteLine("You are unknown");
+            }
+            
+        }
+    }
+}
---
+++ day1/exerciseTwo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace exerciseTwo
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine(GetStatus(2));
+        }
+    public static string GetStatus(int status)
+        {
+            if(status == 1)
+            {
+                return "Alive";
+            }
+            else if(status == 2)
+            {
+                return "Zombie";
+            }
+            else if(status == 3)
+            {
+                return "Dead";
+            }
+            else if(status == 4)
+            {
+                return "Unknown";
+            } 
+
+            return "unknown";
+        }
+    }
+}
+            // while (whileCount < statusCodes.Length)
+            // {
+            //    if(statusCodes[whileCount] == 1)
+            //     {
+            //         Console.WriteLine("1=You are alive");
+            //         whileCount ++;
+            //     }
+            //     else if(statusCodes[whileCount] == 2)
+            //     {
+            //         Console.WriteLine("2=You are a zombie");
+            //         whileCount ++;
+            //     }
+            //     else if(statusCodes[whileCount] == 3)
+            //     {
+            //         Console.WriteLine("3=You are dead");
+            //         whileCount ++;
+            //     }
+            //     else if(statusCodes[whileCount] == 4)
+            //     {
+            //         Console.WriteLine("4=You are unknown");
+            //         whileCount ++;
+            //     } 
+            // }      
+        
+    
+
